3d433da15cd89d306a576e4a4f70a98aa79a8251
castrosuellenx <castrosuellenx@gmail.com>
2022-11-12 01:32:27 -0300

    init Google Auth with expo-auth-session

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,9 +6,12 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
-import {THEME} from './src/styles/theme';
+import {AuthContextProvider} from './src/contexts/AuthContext';
+
 import {Loading} from './src/components/Loading';
 import SignIn from './src/screens/SignIn';
+
+import {THEME} from './src/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,13 +22,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,10 +1,14 @@
 import {Center, Icon, Text} from 'native-base';
 import {Fontisto} from '@expo/vector-icons';
 
+import {useAuth} from '../hooks/useAuth';
+
 import Logo from '../assets/logo.svg';
 import {Button} from '../components/Button';
 
 export default function SignIn() {
+  const {signIn} = useAuth();
+
   return (
     <Center flex={1} bgColor="gray.900" p="7">
       <Logo width={212} height={40} />
@@ -14,6 +18,7 @@ export default function SignIn() {
         title="Entrar com o google"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
+        onPress={signIn}
       />
 
       <Text color="white" textAlign="center" mt={4}>
