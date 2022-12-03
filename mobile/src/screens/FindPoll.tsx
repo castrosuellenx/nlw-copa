4519064440954b90b0f48f21094fc19dd2733d62
castrosuellenx <castrosuellenx@gmail.com>
2022-12-03 20:25:41 -0300

    added function to join poll by code at FindPoll screen

--- mobile/src/screens/FindPoll.tsx
+++ mobile/src/screens/FindPoll.tsx
@@ -1,12 +1,67 @@
+import {useState} from 'react';
+import {Heading, useToast, VStack} from 'native-base';
 import {useNavigation} from '@react-navigation/native';
-import {Heading, VStack} from 'native-base';
 
+import {api} from '../services/api';
 import {Button} from '../components/Button';
 import {Header} from '../components/Header';
 import {Input} from '../components/Input';
 
 export function FindPoll() {
   const {navigate} = useNavigation();
+  const toast = useToast();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [code, setCode] = useState('');
+
+  async function handleJoinPoll() {
+    try {
+      setIsLoading(true);
+
+      if (!code.trim()) {
+        return toast.show({
+          title: 'Informe o código.',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      await api.post('/polls/join', {code});
+
+      toast.show({
+        title: 'Você entrou no bolão com sucesso.',
+        placement: 'top',
+        bgColor: 'green.500',
+      });
+
+      navigate('polls');
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+
+      if (error.response?.data?.message === 'Poll not found.') {
+        return toast.show({
+          title: 'Bolão não encontrado.',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      if (error.response?.data?.message === 'You already joined this poll.') {
+        return toast.show({
+          title: 'Você já está nesse bolão.',
+          placement: 'top',
+          bgColor: 'red.500',
+        });
+      }
+
+      toast.show({
+        title: 'Não foi possível encontrar o bolão.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+  }
 
   return (
     <VStack flex={1} bg="gray.900">
@@ -27,8 +82,19 @@ export function FindPoll() {
           Encontre um bolão através de{'\n'}seu código único
         </Heading>
 
-        <Input mb={2} placeholder="Qual o código do bolão?" />
-        <Button title="Buscar bolão" />
+        <Input
+          mb={2}
+          placeholder="Qual o código do bolão?"
+          value={code}
+          onChangeText={setCode}
+          autoCapitalize="characters"
+        />
+
+        <Button
+          title="Buscar bolão"
+          isLoading={isLoading}
+          onPress={handleJoinPoll}
+        />
       </VStack>
     </VStack>
   );
